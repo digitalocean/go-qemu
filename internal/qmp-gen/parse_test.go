d78a88f218c454deaa534ba8e4af3a8c803dd449
Matt Layher <mlayher@digitalocean.com>
2016-10-18 00:21:36 -0400

    internal/qmp-gen: initial TestGenerate with StatusInfo type

--- internal/qmp-gen/parse_test.go
+++ internal/qmp-gen/parse_test.go
@@ -15,9 +15,162 @@
 package gen
 
 import (
+	"bytes"
+	"go/format"
+	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 )
+
+func TestGenerate(t *testing.T) {
+	tests := []struct {
+		name string
+		in   []byte
+		out  []byte
+	}{
+		{
+			name: "StructInfo",
+			in: []byte(`
+##
+# @StatusInfo:
+#
+# Information about VCPU run state
+#
+# @running: true if all VCPUs are runnable, false if not runnable
+#
+# @singlestep: true if VCPUs are in single-step mode
+#
+# @status: the virtual machine @RunState
+#
+# Since:  0.14.0
+#
+# Notes: @singlestep is enabled through the GDB stub
+##
+{ 'struct': 'StatusInfo',
+  'data': {'running': 'bool', 'singlestep': 'bool', 'status': 'RunState'} }
+			`),
+			out: []byte(`
+// StatusInfo -> StatusInfo (struct)
+
+// StatusInfo implements the "StatusInfo" QMP API type.
+type StatusInfo struct {
+	Running    bool     'json:"running"'
+	Singlestep bool     'json:"singlestep"'
+	Status     RunState 'json:"status"'
+}
+			`),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Replace single quote with backtick to match generated Go
+			// source code
+			tt.out = bytes.TrimSpace(bytes.Replace(tt.out, []byte("'"), []byte("`"), -1))
+
+			// Do not check needed types, third parameter is false
+			source := testGenerate(t, tt.in, false)
+
+			if want, got := tt.out, source; !bytes.Contains(got, want) {
+				t.Fatalf("generated code does not match actual code:\n- want: %s\n-  got: %s",
+					indent(string(want)), indent(string(got)))
+			}
+		})
+	}
+}
+
+func TestGenerateNeededTypes(t *testing.T) {
+	tests := []struct {
+		name string
+		in   []byte
+		out  []byte
+	}{
+		{
+			name: "no output: no command that uses StructInfo",
+			in: []byte(`
+##
+# @StatusInfo:
+#
+# Information about VCPU run state
+#
+# @running: true if all VCPUs are runnable, false if not runnable
+#
+# @singlestep: true if VCPUs are in single-step mode
+#
+# @status: the virtual machine @RunState
+#
+# Since:  0.14.0
+#
+# Notes: @singlestep is enabled through the GDB stub
+##
+{ 'struct': 'StatusInfo',
+  'data': {'running': 'bool', 'singlestep': 'bool', 'status': 'RunState'} }
+			`),
+		},
+		{
+			name: "StatusInfo with query-status command",
+			in: []byte(`
+##
+# @StatusInfo:
+#
+# Information about VCPU run state
+#
+# @running: true if all VCPUs are runnable, false if not runnable
+#
+# @singlestep: true if VCPUs are in single-step mode
+#
+# @status: the virtual machine @RunState
+#
+# Since:  0.14.0
+#
+# Notes: @singlestep is enabled through the GDB stub
+##
+{ 'struct': 'StatusInfo',
+  'data': {'running': 'bool', 'singlestep': 'bool', 'status': 'RunState'} }
+
+##
+# @query-status:
+#
+# Query the run status of all VCPUs
+#
+# Returns: @StatusInfo reflecting all VCPUs
+#
+# Since:  0.14.0
+##
+{ 'command': 'query-status', 'returns': 'StatusInfo' }
+			`),
+			out: []byte(`
+// StatusInfo -> StatusInfo (struct)
+
+// StatusInfo implements the "StatusInfo" QMP API type.
+type StatusInfo struct {
+	Running    bool     'json:"running"'
+	Singlestep bool     'json:"singlestep"'
+	Status     RunState 'json:"status"'
+}
+
+// COMMAND query-status
+			`),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Replace single quote with backtick to match generated Go
+			// source code
+			tt.out = bytes.TrimSpace(bytes.Replace(tt.out, []byte("'"), []byte("`"), -1))
+
+			// Check needed types, third parameter is true
+			source := testGenerate(t, tt.in, true)
+
+			if want, got := tt.out, source; !bytes.Contains(got, want) {
+				t.Fatalf("generated code does not match actual code:\n- want: %s\n-  got: %s",
+					indent(string(want)), indent(string(got)))
+			}
+		})
+	}
+}
 
 func indent(s string) string {
 	return strings.Replace(s, "\n", "\n  ", -1)
@@ -78,4 +231,38 @@ Got:
 			}
 		})
 	}
+}
+
+func testGenerate(t *testing.T, in []byte, checkNeededTypes bool) []byte {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write(in)
+	}))
+	defer s.Close()
+
+	defs, err := readDefinitions(s.URL)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	symbols, err := parse(defs)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	need := symbols
+	if checkNeededTypes {
+		need = neededTypes(symbols)
+	}
+
+	bs, err := renderAPI("templates/", symbols, need)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	formatted, err := format.Source(bs)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	return formatted
 }
