9e600716b7f478e0fc8c55b4578fae0a7aada5c0
Connor Kuehl <ckuehl@digitalocean.com>
2022-12-01 10:42:25 -0600

    do not render JSON-ified QAPI
    It's no longer necessary with the native QAPI parser.

--- internal/qmp-gen/main.go
+++ internal/qmp-gen/main.go
@@ -15,8 +15,6 @@
 package gen
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
 	"go/format"
 	"io/ioutil"
@@ -25,10 +23,9 @@ import (
 const specURL = `https://raw.githubusercontent.com/qemu/qemu/stable-2.11/qapi-schema.json`
 
 var (
-	inputSpec  = flag.String("input", specURL, "Input spec")
-	outputGo   = flag.String("output", "", "Generated code output")
-	outputSpec = flag.String("spec", "", "Generated spec output")
-	templates  = flag.String("templates", "", "Template directory")
+	inputSpec = flag.String("input", specURL, "Input spec")
+	outputGo  = flag.String("output", "", "Generated code output")
+	templates = flag.String("templates", "", "Template directory")
 )
 
 // Generate parses a QMP API spec and generates a Go implementation.
@@ -40,23 +37,6 @@ func Generate() error {
 		return err
 	}
 
-	// Write out the concatenated spec.
-	var spec bytes.Buffer
-
-	// First add a comment with the best guess of version
-	spec.WriteString("\n##QEMU SPECIFICATION VERSION:  ")
-	spec.WriteString(tryGetVersionFromSpecPath(*inputSpec))
-	spec.WriteString("\n\n")
-
-	for _, def := range defs {
-		spec.WriteString(def.Docstring)
-		spec.WriteByte('\n')
-		json.Indent(&spec, def.JSON, "", "  ")
-		spec.WriteString("\n\n")
-	}
-	if err = ioutil.WriteFile(*outputSpec, spec.Bytes(), 0640); err != nil {
-		return err
-	}
 	// Generate and write out the code.
 	symbols, err := parse(defs)
 	if err != nil {
