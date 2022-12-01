b5cadf9c94254d632f35a14e29aa17b228cb2d81
Connor Kuehl <ckuehl@digitalocean.com>
2022-12-01 10:42:25 -0600

    do not render JSON-ified QAPI
    It's no longer necessary with the native QAPI parser.

--- qmp/raw/doc.go
+++ qmp/raw/doc.go
@@ -16,4 +16,4 @@
 // QMP schema.
 package raw
 
-//go:generate go run gen-api.go -output autogen.go -spec spec.txt -templates ../../internal/qmp-gen/templates
+//go:generate go run gen-api.go -output autogen.go -templates ../../internal/qmp-gen/templates
