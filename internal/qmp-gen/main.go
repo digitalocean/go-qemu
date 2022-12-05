0d6614c008c46982fff11cb2257231f1f870567c
Connor Kuehl <ckuehl@digitalocean.com>
2022-12-05 12:41:50 -0600

    convert parse tree to interior types

--- internal/qmp-gen/main.go
+++ internal/qmp-gen/main.go
@@ -15,9 +15,12 @@
 package gen
 
 import (
+	"container/list"
 	"flag"
 	"go/format"
 	"io/ioutil"
+
+	qapischema "github.com/digitalocean/go-qemu/qapi-schema"
 )
 
 const specURL = `https://raw.githubusercontent.com/qemu/qemu/stable-2.11/qapi-schema.json`
@@ -32,16 +35,23 @@ var (
 func Generate() error {
 	flag.Parse()
 
-	defs, err := readDefinitions(*inputSpec)
+	src, err := getQAPI(*inputSpec)
 	if err != nil {
 		return err
 	}
 
-	// Generate and write out the code.
-	symbols, err := parse(defs)
+	tree, err := qapischema.Parse(string(src))
 	if err != nil {
 		return err
 	}
+
+	tree, err = completeParseTree(tree, *inputSpec)
+	if err != nil {
+		return err
+	}
+
+	symbols := lowerParseTree(tree)
+
 	need := neededTypes(symbols)
 	bs, err := renderAPI(*templates, symbols, need)
 	if err != nil {
@@ -56,4 +66,252 @@ func Generate() error {
 	}
 
 	return ioutil.WriteFile(*outputGo, formatted, 0640)
+}
+
+// completeParseTree walks the parse tree and expands the Include statements.
+// It returns the completely resolved parse tree.
+func completeParseTree(tree *qapischema.Tree, base string) (*qapischema.Tree, error) {
+	if inc, ok := tree.Node.(qapischema.Include); ok {
+		src, err := resolvePath(base, string(inc))
+		if err != nil {
+			return nil, err
+		}
+
+		// Any include directives in a subtree of this node will use
+		// *this* include directive as its main path.
+		base = src
+
+		srcBytes, err := getQAPI(src)
+		if err != nil {
+			return nil, err
+		}
+
+		root, err := qapischema.Parse(string(srcBytes))
+		if err != nil {
+			return nil, err
+		}
+
+		tree.Children = root.Children
+	}
+
+	for i, subtree := range tree.Children {
+		st, err := completeParseTree(subtree, base)
+		if err != nil {
+			return tree, err
+		}
+
+		tree.Children[i] = st
+	}
+
+	return tree, nil
+}
+
+// lowerParseTree converts the parsed types from package qapischema into
+// types that are wired up for Go code generation.
+func lowerParseTree(tree *qapischema.Tree) map[name]interface{} {
+	types := make(map[name]interface{})
+
+	remaining := list.New()
+	remaining.PushFront(tree)
+
+	for remaining.Len() > 0 {
+		tree := remaining.Remove(remaining.Front()).(*qapischema.Tree)
+
+		for _, t := range tree.Children {
+			remaining.PushBack(t)
+		}
+
+		switch node := tree.Node.(type) {
+		case qapischema.Root:
+			// Nothing to do, subtrees were already added to the queue above.
+		case qapischema.Include:
+			// Nothing to do, subtrees were already added to the queue above.
+		case qapischema.Pragma:
+			// Ignored by go-qemu.
+		case *qapischema.Struct:
+			v := structType{
+				Name: name(node.Name),
+				Base: name(node.Base),
+			}
+
+			for _, member := range node.Members {
+				f := field{
+					Name:     name(member.Name),
+					Optional: member.Optional,
+				}
+
+				if member.Type.TypeArray != "" {
+					f.Type = name(member.Type.TypeArray)
+					f.List = true
+				} else {
+					f.Type = name(member.Type.Type)
+				}
+				v.Fields = append(v.Fields, f)
+			}
+
+			types[v.Name] = v
+
+		case *qapischema.Command:
+			v := command{
+				Name: name(node.Name),
+			}
+
+			if node.Boxed != nil {
+				v.BoxedInput = *node.Boxed
+			}
+
+			if node.Data.Ref != nil {
+				v.Inputs.Ref = name(*node.Data.Ref)
+			} else if node.Data.Embed != nil {
+				var fs fields
+				for _, member := range node.Data.Embed.Members {
+					f := field{
+						Name:     name(member.Name),
+						Optional: member.Optional,
+					}
+
+					if member.Type.TypeArray != "" {
+						f.List = true
+						f.Type = name(member.Type.TypeArray)
+					} else {
+						f.Type = name(member.Type.Type)
+					}
+
+					fs = append(fs, f)
+					v.Inputs.AnonFields = fs
+				}
+			}
+
+			if node.Returns != nil {
+				// TODO: Output.Name is confusing
+				if node.Returns.TypeArray != "" {
+					v.Output = field{
+						Name: name(node.Returns.TypeArray),
+						List: true,
+						Type: name(node.Returns.TypeArray),
+						// TODO: Optional:
+					}
+				} else {
+					v.Output = field{
+						Name: name(node.Returns.Type),
+						Type: name(node.Returns.Type),
+						// TODO: Optional:
+					}
+				}
+			}
+
+			types[v.Name] = v
+		case *qapischema.Event:
+			v := event{
+				Name: name(node.Name),
+			}
+
+			if node.Data.Selector.Embed != nil {
+				v.Data = fieldsOrRef{
+					Ref: name(node.Data.Selector.Ref),
+				}
+				if v.Data.Ref != "" {
+					v.BoxedData = true
+				}
+
+				for _, member := range node.Data.Selector.Embed.Members {
+					f := field{
+						Name:     name(member.Name),
+						Optional: member.Optional,
+					}
+
+					if member.Type.TypeArray != "" {
+						f.List = true
+						f.Type = name(member.Type.TypeArray)
+					} else {
+						f.Type = name(member.Type.Type)
+					}
+
+					v.Data.AnonFields = append(v.Data.AnonFields, f)
+				}
+			}
+
+			types[v.Name] = v
+		case *qapischema.Alternate:
+			v := alternate{
+				Name:    name(node.Name),
+				Options: make(map[name]name),
+			}
+
+			for _, opt := range node.Data {
+				n := opt.Type.Type
+				if opt.Type.TypeArray != "" {
+					n = string(opt.Type.TypeArray)
+				}
+				v.Options[name(opt.Name)] = name(n)
+			}
+
+			types[v.Name] = v
+		case *qapischema.Union:
+			if node.Discriminator == "" {
+				v := simpleUnion{
+					Name:    name(node.Name),
+					Options: make(map[name]name),
+				}
+
+				for _, branch := range node.Branches {
+					b := string(branch.Type.Type)
+					if branch.Type.TypeArray != "" {
+						b = string(branch.Type.TypeArray)
+					}
+					v.Options[name(branch.Name)] = name(b)
+				}
+
+				types[v.Name] = v
+			} else {
+				v := flatUnion{
+					Name:          name(node.Name),
+					Discriminator: name(node.Discriminator),
+					Base: fieldsOrRef{
+						Ref: name(node.Base.Name),
+					},
+					Options: make(map[name]name),
+				}
+
+				for _, member := range node.Base.Members {
+					f := field{
+						Name:     name(member.Name),
+						Optional: member.Optional,
+					}
+
+					if member.Type.TypeArray != "" {
+						f.List = true
+						f.Type = name(member.Type.TypeArray)
+					} else {
+						f.Type = name(member.Type.Type)
+					}
+
+					v.Base.AnonFields = append(v.Base.AnonFields, f)
+				}
+
+				for _, branch := range node.Branches {
+					b := string(branch.Type.Type)
+					if branch.Type.TypeArray != "" {
+						b = string(branch.Type.TypeArray)
+					}
+					v.Options[name(branch.Name)] = name(b)
+				}
+
+				types[v.Name] = v
+			}
+
+		case *qapischema.Enum:
+			v := enum{
+				Name: name(node.Name),
+			}
+
+			for _, val := range node.Values {
+				v.Values = append(v.Values, name(val.Value))
+			}
+
+			types[v.Name] = v
+		}
+	}
+
+	return types
 }
